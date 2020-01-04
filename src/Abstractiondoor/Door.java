970e695d455a8727df57357201c79e459f75e569
Acer <Acer@Acer-PC>
2020-01-04 17:25:42 +0530

    ABSTRACTION:
    	In this application create a five classes and find the palindrome and
    pattern, fibonacci of the values.

---
+++ src/Abstractiondoor/Door.java
@@ -0,0 +1,20 @@
+package Abstractiondoor;
+
+	public abstract class Door {
+		protected String doorName;
+		public Door(String doorName) {
+		super();
+		this.doorName = doorName;
+		}
+		public String getDoorName() {
+		return doorName;
+		}
+		public void setDoorName(String doorName) {
+		this.doorName = doorName;
+		}
+
+
+		}
+
+
+
